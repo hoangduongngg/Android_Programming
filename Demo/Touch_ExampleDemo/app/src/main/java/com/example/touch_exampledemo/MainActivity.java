cd5f0e7577d053d232aebd9a1f5aa67aec529046
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2023-03-22 15:47:41 +0700

    Touch Example Demo

--- Demo/Touch_ExampleDemo/app/src/main/java/com/example/touch_exampledemo/MainActivity.java
+++ Demo/Touch_ExampleDemo/app/src/main/java/com/example/touch_exampledemo/MainActivity.java
@@ -3,12 +3,56 @@ package com.example.touch_exampledemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText e1,e2,e3,e4;
+    private ImageView img;
+
+    private float xDown = 0, yDown = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        img.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        xDown = event.getX();
+                        yDown = event.getY();
+                        e1.setText(xDown+ "");
+                        e2.setText(yDown+ "");
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        float moveX = event.getX();
+                        float moveY = event.getY();
+                        e3.setText(moveX+ "");
+                        e4.setText(moveY+ "");
+
+                        //Do dich chuyen
+                        float dx = moveX - xDown;
+                        float dy = moveY - yDown;
+                        img.setX(img.getX() + dx);
+                        img.setY(img.getY() + dy);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initView() {
+        e1 = findViewById(R.id.txt1);
+        e2 = findViewById(R.id.txt2);
+        e3 = findViewById(R.id.txt3);
+        e4 = findViewById(R.id.txt4);
+        img = findViewById(R.id.img);
     }
 }
